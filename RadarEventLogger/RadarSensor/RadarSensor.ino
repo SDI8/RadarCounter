385f632903320480388927a09fd0892bd2d9a584
SDITrifork <sdi@trifork.com>
2016-10-28 22:13:37 +0200

    Initial.

---
+++ RadarEventLogger/RadarSensor/RadarSensor.ino
@@ -0,0 +1,124 @@
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266mDNS.h>
+
+#include "passwords.h"
+
+#define FREQ_PIN 5
+#define LED_PIN 2
+
+boolean state = false;
+volatile uint32_t cycle_start_time;
+volatile uint32_t cycle_low_time;
+uint32_t cycle_high_time;
+uint32_t cycle_time;
+
+volatile float hilo_ratio_err;
+float avg_hilo_ratio_err;
+volatile float freq;
+float avg_freq;
+
+ESP8266WebServer server(80);
+
+void freqCounterRisingISR( void ) {
+  cycle_low_time = abs( micros() - cycle_start_time );
+}
+
+void freqCounterFallingISR( void ) {
+  cycle_high_time = abs(micros() - cycle_start_time) - cycle_low_time;
+  cycle_start_time = micros();
+
+  cycle_time = cycle_low_time + cycle_high_time;
+
+  if (cycle_time > 0) {
+    hilo_ratio_err = abs( cycle_low_time - cycle_high_time ) / (cycle_time / 100);
+    freq = 1000000 / cycle_time;
+  }
+        
+}
+
+void freqCounterISR( void ) {
+  if (digitalRead(FREQ_PIN)) {
+    freqCounterRisingISR();
+    digitalWrite(LED_PIN, HIGH);
+  } else {
+    freqCounterFallingISR();
+    digitalWrite(LED_PIN, LOW);
+  }
+}
+
+void handleSimpleRequest( void ) {
+
+  String message = String( avg_hilo_ratio_err );
+  message += "\t";
+  message += String( avg_freq );
+  server.send(200, "text/plain", message);
+
+}
+
+float toVoltage(int x) {
+  return 29.8328+(-0.0762457+0.0000557642*x)*x;
+}
+
+void handleState() {
+  int raw = analogRead(A0);
+  String message = String(raw);
+  message += "\t";
+  message += String(toVoltage(raw),4);
+  message += "V";
+  server.send(200, "text/plain", message);
+}
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode( 16, LOW );
+  pinMode( FREQ_PIN, INPUT);
+  pinMode( LED_PIN, OUTPUT );
+  attachInterrupt(digitalPinToInterrupt(FREQ_PIN), freqCounterISR, CHANGE);
+  Serial.begin(115200);
+
+  WiFi.begin(ssid, password);
+  
+  // Wait for connection
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  if (MDNS.begin("radarsensor")) {
+    Serial.println("MDNS responder started");
+  }
+  server.on("/", handleSimpleRequest);
+  server.on("/state", handleState);
+  server.onNotFound([](){server.send(404,"text/plain","404");});
+  server.begin();
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  noInterrupts();
+  if (freq < 1000) {
+    avg_freq = avg_freq * .9 + freq * .1;  
+  }
+  if (hilo_ratio_err <= 100) {
+    avg_hilo_ratio_err = avg_hilo_ratio_err * .9 + hilo_ratio_err * .1;
+  }
+  interrupts();
+/*
+  Serial.print( avg_hilo_ratio_err );
+  Serial.print("\t");
+  Serial.println( avg_freq );
+*/
+  delay(20);
+  server.handleClient();
+}
+
+
---
+++ RadarEventLogger/VoltageServer/VoltageServer.ino
@@ -0,0 +1,118 @@
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266mDNS.h>
+#include "FS.h"
+#include <ArduinoJson.h>
+
+#include "passwords.h"
+
+uint32_t lastcheck = 0;
+
+ESP8266WebServer server(80);
+
+const int led = 13;
+
+float toVoltage(int x) {
+  return 29.8328+(-0.0762457+0.0000557642*x)*x;
+}
+
+void handleRoot() {
+  digitalWrite(led, 1);
+  File logfile = SPIFFS.open("/log.json", "r");
+  if (logfile) {
+    server.streamFile(logfile, "application/json");
+  }
+  logfile.close();
+  digitalWrite(led, 0);
+}
+
+void handleDirect() {
+  digitalWrite(led, 1);
+  int raw = analogRead(A0);
+  String message = String(raw);
+  message += "\t";
+  message += String(toVoltage(raw),4);
+  message += "V";
+  server.send(200, "text/plain", message);
+  digitalWrite(led, 0);
+}
+
+bool saveState() {
+  StaticJsonBuffer<200> jsonBuffer;
+  JsonObject& json = jsonBuffer.createObject();
+  json["time"] = String(millis() / 1000);
+  json["value"] = (analogRead(A0) / 199.0f) *(-1) + 5.14;
+
+  File logfile = SPIFFS.open("/log.json", "a");
+  if (!logfile) {
+    Serial.println("Failed to open config file for writing");
+    return false;
+  }
+
+  json.printTo(logfile);
+  logfile.print(",\n");
+  logfile.close();
+  return true;
+}
+
+void handleNotFound(){
+  digitalWrite(led, 1);
+  String message = "File Not Found\n\n";
+  message += "URI: ";
+  message += server.uri();
+  message += "\nMethod: ";
+  message += (server.method() == HTTP_GET)?"GET":"POST";
+  message += "\nArguments: ";
+  message += server.args();
+  message += "\n";
+  for (uint8_t i=0; i<server.args(); i++){
+    message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
+  }
+  server.send(404, "text/plain", message);
+  digitalWrite(led, 0);
+}
+
+void setup(void){
+  pinMode(led, OUTPUT);
+  digitalWrite(led, 0);
+  Serial.begin(115200);
+  WiFi.begin(ssid, password);
+  Serial.println("");
+  SPIFFS.begin();
+  // Wait for connection
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  if (MDNS.begin("esp8266")) {
+    Serial.println("MDNS responder started");
+  }
+
+  server.on("/", handleRoot);
+  server.on("/direct", handleDirect);
+
+  server.on("/inline", [](){
+    server.send(200, "text/plain", "this works as well");
+  });
+
+  server.onNotFound(handleNotFound);
+
+  server.begin();
+  Serial.println("HTTP server started");
+}
+
+void loop(void){
+  server.handleClient();
+
+  if ((lastcheck + 60) < millis()/1000) {
+    lastcheck = millis()/1000;
+    saveState();
+  }
+}
